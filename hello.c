66480f78409495628fb9065db1e8fc0c2fbd33b1
shusso <hussoster@gmail.com>
2012-01-26 09:03:12 +0200

    added goodbye feature

--- hello.c
+++ hello.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void goodbye()
+{
+    printf("Goodbye cruel world\n");
+}
+
+
 int main(void)
 {
     printf("Hello Wordl!!!\n");
+    goodbye();
     return EXIT_SUCCESS;
 }
